bc35ea4a92996215b8361b63a0b702800fef46ef
ひろ <hiroyuki09171016@gmail.com>
2023-04-06 20:07:54 +0900

    トップページの変更

--- portfoliosite/config/urls.py
+++ portfoliosite/config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nippo/', include('nippo.urls')),
+    # path('nippo/', include('nippo.urls')),
+    path('', include('nippo.urls')),
 ]
