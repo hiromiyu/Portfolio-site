ce967b84f9ef9a03bcd5beb1fce7d36e4b2c853d
ひろ <hiroyuki09171016@gmail.com>
2023-04-09 16:24:24 +0900

    リリースへの細かい修正

--- portfoliosite/nippo/forms.py
+++ portfoliosite/nippo/forms.py
@@ -20,13 +20,13 @@ class InquiryForm(forms.Form):
         super().__init__(*args, **kwargs)
 
         self.fields['name'].widget.attrs['class'] = 'form-control'
-        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください。'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前を入力してください。'
         self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください。'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスを入力してください。'
         self.fields['title'].widget.attrs['class'] = 'form-control'
-        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルを入力してください。'
         self.fields['message'].widget.attrs['class'] = 'form-control'
-        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージを入力してください。'
 
     def send_email(self):
         name = self.cleaned_data['name']
